f995ca7d296f0fa90ed2ec24af3f188a35604ab1
hassank <hassank@stanford.edu>
2014-02-06 02:56:09 -0800

    Commit sliding full pages

--- tabbedSliders/js/common.js
+++ tabbedSliders/js/common.js
@@ -14,18 +14,17 @@ $(function (){
 				//check which tab has been clicked
 				var getID = $(this).attr("id");
 				if(getID == "contact"){
-					$("#formWrapper ul").animate({left: "0px"}, { duration: 350, easing: 'easeInOutCirc' });
+					$("#formWrapper ul").animate({left: "0px"}, { duration: 1000, easing: 'easeInOutCirc' });
 					ClearClasses();
 					$(this).parent("LI").addClass("current");
 				} else if(getID == "quote"){
-					$("#formWrapper ul").animate({left: "-680px"}, { duration: 350, easing: 'easeInOutCirc' });
+					$("#formWrapper ul").animate({left: "-99%"}, { duration: 1000, easing: 'easeInOutCirc' });
 					ClearClasses();
 					$(this).parent("LI").addClass("current");			
 				} else if(getID == "demo"){
-					$("#formWrapper ul").animate({left: "-1360px"}, { duration: 350, easing: 'easeInOutCirc' });
+					$("#formWrapper ul").animate({left: "-199%"}, { duration: 1000, easing: 'easeInOutCirc' });
 					ClearClasses();
 					$(this).parent("LI").addClass("current");
 				}
 			});
-	
 	});
