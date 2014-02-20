6d4761b3bf0a13e06d6161afe76cc1ed1d2cc1af
Lingtong Sun <lsun569@stanford.edu>
2014-02-20 15:46:29 -0800

    moar shit

--- routes/chat.js
+++ routes/chat.js
@@ -1,6 +1,19 @@
 var data = require('../users.json');
 
+var models = require('../models');
+
 exports.view = function(req, res){
   console.log(data);
+<<<<<<< HEAD
   res.render('chat', { "users" : data });
+=======
+
+  var current_user = "0";
+  var db_data = models.Friend
+  					.find("user_one": current_user)
+  					.exec(afterFindFriends);
+
+  
+  res.render('chat', data);
+>>>>>>> d20e9da... moar shit
 };
