633debf3e3f740b352292376f46722ec636cbc7c
hassank <hassank@stanford.edu>
2014-02-20 15:11:37 -0800

    Fixed login flow and modifying mongo

--- routes/project.js
+++ routes/project.js
@@ -1,7 +1,21 @@
-exports.viewProject = function(req, res) { 
- 	var name = req.params.name; 
-  	console.log("The project name is: " + name);
-	res.render('project', {
-		'projectName' : name
-	});
-};
+var models = require('../models');
+
+exports.addFriendship = function(req, res) {
+	var friendship = req.body;
+	console.log(friendship);
+
+	// make a new Friend and save it to the DB
+	// MUST send an OK response w/ res.send();
+
+	var newFriendship = new models.Friendship({
+    	"user_one" : friendship['user_one'],
+		"user_two" : friendship['user_two']
+  	});
+
+  	newFriendship.save(afterSaving);
+
+  	function afterSaving(err) {
+    	if(err) { console.log(err); res.send(500); }
+    	res.send();
+  	}	
+}
