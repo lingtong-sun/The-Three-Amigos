6ef5f9e1c454932c4035314d860f9eaf0da4570b
Emily Guth <eaguth@stanford.edu>
2014-02-20 15:07:51 -0800

    autcomplete stuff not working

--- public/js/clickable.js
+++ public/js/clickable.js
@@ -5,6 +5,18 @@ $(document).ready(function() {
 	$(".convo-link").on("click", function() {
 		window.document.location = $(this).attr("href");
 	});
+
+
+  console.log("hello?");
+
+$('input.counties').typeahead({
+      name: 'countries',
+      local: ["Unites States", "Mexico", "Canada", "Cuba", "Guatemala"]
+}).on('typeahead:selected', function(event, datum) {
+	console.log("meow");
+});
+
+
 })
 
 
