15f16aafff91e3213dc09d6f7514a7c987a86c5d
Lingtong Sun <lsun569@stanford.edu>
2014-02-20 15:42:12 -0800

    initDB.js

--- initDB.js
+++ initDB.js
@@ -43,53 +43,79 @@ function onceUsersClear(err) {
     var json = users_json[i];
     var user = new models.User(json);
 
-    user.save(function(err, proj) {
+    user.save(function(err, user) {
       if(err) console.log(err);
 
       to_save_count--;
-      console.log(to_save_count + ' left to save');
+      console.log(to_save_count + 'users left to save');
       if(to_save_count <= 0) {
         console.log('DONE');
-        // The script won't terminate until the 
-        // connection to the database is closed
-        mongoose.connection.close()
       }
     });
   }
 }
-
-
-
-
-
 
 
 
 // Step 1: load the JSON data
-var friends_json = require('./users.json');
+var friends_json = require('./friends.json');
 
 // Step 2: Remove all existing documents
-models.User
+models.Friend
   .find()
   .remove()
-  .exec(onceUsersClear); // callback to continue at
+  .exec(onceFriendsClear); // callback to continue at
 
 // Step 3: load the data from the JSON file
-function onceUsersClear(err) {
+function onceFriendsClear(err) {
   if(err) console.log(err);
 
   // loop over the projects, construct and save an object from each one
   // Note that we don't care what order these saves are happening in...
-  var to_save_count = users_json.length;
-  for(var i=0; i<users_json.length; i++) {
-    var json = users_json[i];
-    var user = new models.User(json);
+  var to_save_count = friends_json.length;
+  for(var i=0; i<friends_json.length; i++) {
+    var json = friends_json[i];
+    var friend = new models.Friend(json);
 
-    user.save(function(err, proj) {
+    friend.save(function(err, friend) {
       if(err) console.log(err);
 
       to_save_count--;
-      console.log(to_save_count + ' left to save');
+      console.log(to_save_count + 'friends left to save');
+      if(to_save_count <= 0) {
+        console.log('DONE');
+      }
+    });
+  }
+}
+
+
+
+// Step 1: load the JSON data
+var messages_json = require('./messages.json');
+
+// Step 2: Remove all existing documents
+models.Message
+  .find()
+  .remove()
+  .exec(onceMessagesClear); // callback to continue at
+
+// Step 3: load the data from the JSON file
+function onceMessagesClear(err) {
+  if(err) console.log(err);
+
+  // loop over the projects, construct and save an object from each one
+  // Note that we don't care what order these saves are happening in...
+  var to_save_count = messages_json.length;
+  for(var i=0; i<messages_json.length; i++) {
+    var json = messages_json[i];
+    var message = new models.Message(json);
+
+    message.save(function(err, message) {
+      if(err) console.log(err);
+
+      to_save_count--;
+      console.log(to_save_count + 'messages left to save');
       if(to_save_count <= 0) {
         console.log('DONE');
         // The script won't terminate until the 
@@ -99,4 +125,6 @@ function onceUsersClear(err) {
     });
   }
 }
+
+
 
