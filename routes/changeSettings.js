89d0e9a7d5252eeae7fb0ed2babdcb44b7f18941
Emily Guth <eaguth@stanford.edu>
2014-02-21 01:40:47 -0800

    trying to get change settings to work

--- routes/changeSettings.js
+++ routes/changeSettings.js
@@ -1,5 +1,22 @@
+var models = require('../models');
+
+
 exports.changeSettings = function(req, res){
-  console.log(req['body']);
+	var body =  req['body'];
+	console.log(body);
+	console.log(body.sendlang);
 
+	var current_user = req.session.user_id;
 
+	models.User
+	.find({"_id" : current_user})
+	.update({"send_language": body.sendlang,
+		"receive_language": body.recievelang })
+	.exec(afterUpdating);
+
+	function afterUpdating(err) {
+		if(err) console.log(err);
+		res.send();
+
+	}
 };
