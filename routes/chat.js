884e2f7ae21bd223065060489456c48142dd43ff
Lingtong Sun <lsun569@stanford.edu>
2014-02-21 01:00:32 -0800

    current profile

--- routes/chat.js
+++ routes/chat.js
@@ -8,6 +8,20 @@ exports.view = function(req, res){
   var current_user = req.session.user_id;
   var db_data = new Array();
   var db_messages = new Array();
+
+  var my_profile;
+  models.User 
+		  .findOne({"facebook_id": current_user})
+		  .exec(setFriend);
+  function setFriend(err, me) {
+   	if (err) console.log(err);
+   	my_profile = me;
+   	console.log(me);
+   	//assume this will never finish last
+  }
+
+
+
   models.Friend
   		.find({"user_one": current_user})
   		.exec(afterFindFriends);
@@ -34,8 +48,6 @@ exports.view = function(req, res){
     	function afterGrabbingUserData(err, users) {
     		if(err) console.log(err);
     		//console.log(users);
-    		//var stuff = {};
-    		//stuff[users[0]['facebook_id']] = users[0];
     		db_data[counter] = users[0];
     		var friend_json = friends[counter];
     		counter ++;
@@ -56,22 +68,24 @@ exports.view = function(req, res){
     				console.log(message_counter + ": " + counter);
     				counter_dup ++;
     				message_counter--;
-    				if (message_counter == 0 && counter == friends.length) {
+    				if (message_counter == 0 && counter == friends.length && my_profile!=undefined) {
     					console.log(db_data);
     					console.log(db_messages);
    						res.render('chat', { "users" : db_data, 
-   									 "messages" : db_messages });
+   									 "messages" : db_messages ,
+   									 "curr_user": my_profile});
     				}	
     			}
     		} else {
     			counter_dup ++; //if no messages
     		}
 
-    		if (counter == friends.length && message_counter == 0) {
+    		if (counter == friends.length && message_counter == 0 && my_profile!=undefined) {
     			console.log(db_data);
     			console.log(db_messages);
    				res.render('chat', { "users" : db_data, 
-   									 "messages" : db_messages });
+   									 "messages" : db_messages ,
+   									 "curr_user": my_profile});
     		}
     	}
 
--- routes/conversation.js
+++ routes/conversation.js
@@ -1,12 +1,56 @@
-var data = require('../messages.json');
+
+var models = require('../models');
 
 exports.viewConversation = function(req, res){
-	var name = req.params.name;
+	var friend_id = req.params.id;
+	var my_id = req.session.user_id;
 
-  	console.log("The name is: " + name);
-  	console.log(data);
-  	res.render('conversation', {
-  		'name' : name,
-  		'data' : data
-  	});
+	var db_messages = new Array();
+
+	var friend_profile;
+	var my_profile;
+
+	models.User 
+		  .findOne({"facebook_id": friend_id})
+		  .exec(setFriend);
+    function setFriend(err, friend) {
+    	if (err) console.log(err);
+    	console.log(friend);
+    	friend_profile = friend;
+    }
+
+	models.User 
+		  .findOne({"facebook_id": my_id})
+		  .exec(setFriend);
+    function setFriend(err, me) {
+    	if (err) console.log(err);
+    	my_profile = me;
+    	console.log(me);
+    }
+
+	models.Friend
+		.findOne({"user_one": my_id,
+				"user_two": friend_id})
+		.exec(afterFindFriend);
+
+	function afterFindFriend(err, friend) {
+		if(err) console.log(err);
+		console.log(friend);
+
+		models.Message
+			.find({"conversation": friend['conversation_id']})
+			.sort("-send_time")
+			.exec(afterFindMessages);
+
+		function afterFindMessages(err, messages) {
+			if(err) console.log(err);
+			console.log(messages);
+			db_messages = messages;
+		}
+	}
+
+  	console.log(friend_id + ", " + my_id);
+  	// res.render('conversation', {
+  	// 	'data' : data
+  	// });
 };
