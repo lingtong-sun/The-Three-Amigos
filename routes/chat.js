281845526aba6d21019ae027085137ea17752f4e
hassank <hassank@stanford.edu>
2014-02-21 10:33:46 -0800

    push for mini-test

--- routes/chat.js
+++ routes/chat.js
@@ -2,8 +2,8 @@
 var models = require('../models');
 
 exports.view = function(req, res){
-  // console.log(req.session.user_name + " has ID: " + req.session.user_id);
-  // var friendList = req.session.friends.split(',');
+  console.log(req.session.user_name + " has ID: " + req.session.user_id);
+  var friendList = req.session.friends.split(',');
   //console.log("Friends: " + friendList);
 
   var current_user = req.session.user_id;
@@ -11,80 +11,6 @@ exports.view = function(req, res){
   var db_users;
   var db_messages = new Array();
   var my_profile;
-
-  models.User
-    .findOne({"_id": current_user})
-    .exec(checkIfExists);
-
-  function checkIfExists(err, me) {
-      if(err) console.log(err);
-      if(me === null) {
-        console.log("Creating new user");
-
-        var newUser = new models.User ({
-          "_id": req.session.user_id,
-          "name" : req.session.user_name,
-          "send_language" : "en",
-          "receive_language" : "es",
-          "image" : "http://jasonlefkowitz.net/wp-content/uploads/2013/07/cats-16140154-1920-1080.jpg"
-        });
-        newUser.save(afterAddUser);
-
-        function afterAddUser(err) {
-          // check if user has been added, then adjust friendships
-          console.log("NumFriends" + friendList.length);
-          for(var i = 0; i < friendList.length; i++) {
-            // iterate over friend list and add friendships in both directions
-            //console.log(friendList[i]);
-            models.User
-              .findOne({"_id": friendList[i]})
-              .exec(checkIfFriendUsesSpanglish);
-
-            function checkIfFriendUsesSpanglish(err, friend) {
-              if(err) console.log(err);
-              if (friend === null) {
-                //console.log("Friend does not use spanglish");
-              } else {
-                //console.log(friend.name + " uses Spanglish --> create friendship!");
-                var newFriend = new models.Friend ({
-                  "user_one" : req.session.user_id,
-                  "user_two" : friend.id,
-                  "conversation_id" : -1
-                });
-                newFriend.save(afterAddFriend);
-                function afterAddFriend(err) {
-                  models.Friend
-                    .findOne({"user_one": req.session.user_id})
-                    .exec(checkFriend);
-
-                    function checkFriend(err, friendship) {
-                      console.log(req.session.user_name + " friends with " + friendship.user_two);
-                    }
-                }
-
-                var newFriendAlt = new models.Friend ({
-                  "user_one" : friend.id,
-                  "user_two" : req.session.user_id,
-                  "conversation_id" : -1
-                });
-                newFriendAlt.save(afterAddFriendAlt);
-                function afterAddFriendAlt(err) {
-                  models.Friend
-                    .findOne({"user_two": req.session.user_id})
-                    .exec(checkFriend);
-
-                    function checkFriend(err, friendship) {
-                      console.log(friendship.user_one + " friends with " + req.session.user_name);
-                    }
-                } 
-              }
-            }
-          }
-        }
-      }
-    }
-    console.log("Passing new user functions");
-
 
   var message_counter = 0;
   models.User 
--- routes/setSession.js
+++ routes/setSession.js
@@ -2,8 +2,6 @@ var models = require('../models');
 
 exports.setSession = function(req, res){
 
-  
-
 	var id = req.params.id;
 	var name = req.params.name;
 	console.log("In setSession - " + id + " and named: " + name);
@@ -11,37 +9,85 @@ exports.setSession = function(req, res){
   req.session.user_id = id;
   req.session.user_name = name;
   req.session.friends = req.params.arr;
+
+  var friendList = req.session.friends.split(',');
   console.log(req.params.arr.length);
+  var current_user = req.session.user_id;
+  console.log(current_user);
+  
+  models.User
+    .findOne({"_id": current_user})
+    .exec(checkIfExists);
 
-  	//console.log(name);
-  	/*
-  	// now check if the user exists in the DB
-  	models.User
-    		  .find({"facebook_id": id})
-    		  .exec(afterGrabbingUser);
+  function checkIfExists(err, me) {
+      if(err) console.log(err);
+      if(me === null) {
+        console.log("Creating new user");
 
-    function afterGrabbingUser(err, users) {
-    		if(err) console.log(err);
-    		console.log(users);
+        var newUser = new models.User ({
+          "_id": req.session.user_id,
+          "name" : req.session.user_name,
+          "send_language" : "en",
+          "receive_language" : "es",
+          "image" : "http://jasonlefkowitz.net/wp-content/uploads/2013/07/cats-16140154-1920-1080.jpg"
+        });
+        newUser.save(afterAddUser);
 
-    		if (users.length === 0) {
-    			console.log("Must create new user, id: " + id);
+        function afterAddUser(err) {
+          // check if user has been added, then adjust 
+          var counter = 2 * friendList.length;
+          console.log("NumFriends" + friendList.length);
+          for(var i = 0; i < friendList.length; i++) {
+            // iterate over friend list and add friendships in both directions
+            //console.log(friendList[i]);
+            models.User
+              .findOne({"_id": friendList[i]})
+              .exec(checkIfFriendUsesSpanglish);
 
-    			var newUser = new models.Friend({
-    				"facebook_id": id,
-					"name" : String,
-					"send_language" : String,
-					"receive_language" : String,
-					"image" : String
+            function checkIfFriendUsesSpanglish(err, friend) {
+              if(err) console.log(err);
+              if (friend === null) {
+                counter -=2;
+                if(counter == 0) {
+                  res.redirect("/chat");
+                }
+                //console.log("Friend does not use spanglish");
+              } else {
+                //console.log(friend.name + " uses Spanglish --> create friendship!");
+                var newFriend = new models.Friend ({
+                  "user_one" : req.session.user_id,
+                  "user_two" : friend.id,
+                  "conversation_id" : -1
+                });
+                newFriend.save(afterAddFriend);
 
-    			})
-
-
-
-    		}
+                var newFriendAlt = new models.Friend ({
+                  "user_one" : friend.id,
+                  "user_two" : req.session.user_id,
+                  "conversation_id" : -1
+                });
+                newFriendAlt.save(afterAddFriend);
+                function afterAddFriend(err) {
+                  // models.Friend
+                  //   .findOne({"user_two": req.session.user_id})
+                  //   .exec(checkFriend);
+                  counter --;
+                  if (counter ==0) {
+                    res.redirect("/chat");
+                  }
+                  //   function checkFriend(err, friendship) {
+                  //     console.log(friendship.user_one + " friends with " + req.session.user_name);
+                  //   }
+                } 
+              }
+            }
+          }
+        }
+      }
     }
-	*/
+    console.log("Passing new user functions");
+
 
     // by this point, either a new user has been created or the new user has been fetched
-  res.redirect("/chat");
+  //res.redirect("/chat");
 };
